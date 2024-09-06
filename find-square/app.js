fbebc36a5cc4d9ea5feeeba5ced863d842af42b7
dmitrytrofim <78739257+dmitrytrofim@users.noreply.github.com>
2024-09-06 17:10:57 +0300

    add find block

--- find-square/app.js
+++ find-square/app.js
@@ -3,8 +3,8 @@ class App {
   this.canvas = document.getElementById('canvas');
   this.ctx = this.canvas.getContext('2d');
   this.paramsCanvas = {
-   width: window.innerWidth - 10,
-   height: window.innerHeight - 10,
+   width: null,
+   height: null,
   };
   this.block = {
    startX: 0,
@@ -12,32 +12,91 @@ class App {
    width: 100,
    height: 100,
   };
+  this.velocity = 20;
+  this.findBlock = {
+   startX: 200,
+   startY: 200,
+   width: 100,
+   height: 100,
+  };
+  this.direction = {
+   up: false,
+   down: false,
+   left: false,
+   right: false,
+  };
+ }
+ randomFunc(min, max) {
+  return Math.floor(Math.random() * (max - min) + min);
+ }
+ randomFindBlock(x, y) {
+  this.findBlock.startX = this.randomFunc(
+   200,
+   this.paramsCanvas.width - this.findBlock.width
+  );
+  this.findBlock.startY = this.randomFunc(
+   200,
+   this.paramsCanvas.height - this.findBlock.height
+  );
  }
  setParams() {
   const setFucn = () => {
+   this.paramsCanvas.width = window.innerWidth - 10;
+   this.paramsCanvas.height = window.innerHeight - 10;
    this.canvas.width = this.paramsCanvas.width;
    this.canvas.height = this.paramsCanvas.height;
-   this.drawCanvas();
+   this.block.startX = 0;
+   this.block.startY = 0;
+   this.randomFindBlock();
   };
-  window.addEventListener('load', setFucn);
+  window.addEventListener('DOMContentLoaded', setFucn);
   window.addEventListener('resize', setFucn);
  }
  drawCanvas() {
+  if (this.direction.up && this.block.startY > 0)
+   this.block.startY -= this.velocity;
+  if (
+   this.direction.down &&
+   this.block.startY + this.block.height < this.paramsCanvas.height
+  )
+   this.block.startY += this.velocity;
+  if (
+   this.direction.right &&
+   this.block.startX + this.block.width < this.paramsCanvas.width
+  )
+   this.block.startX += this.velocity;
+  if (this.direction.left && this.block.startX > 0)
+   this.block.startX -= this.velocity;
+  this.ctx.clearRect(0, 0, this.paramsCanvas.width, this.paramsCanvas.height);
   this.ctx.fillRect(...Object.values(this.block));
-  this.canvas.addEventListener('click', (e) => {
-   if (
-    e.offsetX >= this.block.startX &&
-    e.offsetX <= this.block.width &&
-    e.offsetY >= this.block.startY &&
-    e.offsetY <= this.block.height
-   ) {
-    console.log('yep');
-   }
+  this.ctx.fillRect(...Object.values(this.findBlock));
+  requestAnimationFrame(() => {
+   this.drawCanvas();
   });
+ }
+ controlPanel() {
+  const handleClick = (e) => {
+   if (e.type === 'keydown') {
+    if (e.code === 'ArrowUp') this.direction.up = true;
+    if (e.code === 'ArrowDown') this.direction.down = true;
+    if (e.code === 'ArrowRight') this.direction.right = true;
+    if (e.code === 'ArrowLeft') this.direction.left = true;
+   }
+   if (e.type === 'keyup') {
+    if (e.code === 'ArrowUp') this.direction.up = false;
+    if (e.code === 'ArrowDown') this.direction.down = false;
+    if (e.code === 'ArrowRight') this.direction.right = false;
+    if (e.code === 'ArrowLeft') this.direction.left = false;
+   }
+  };
+  window.addEventListener('keydown', handleClick);
+  window.addEventListener('keyup', handleClick);
  }
  start() {
   if (this.canvas) {
    this.setParams();
+   this.controlPanel();
+   this.drawCanvas();
   }
  }
 }
